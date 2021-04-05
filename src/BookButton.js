3f255fb23e3135311a079efbb427f2079e909330
Jacob Thomas <jcthomas00@gmail.com>
2021-04-05 09:42:33 -0500

    Modified BookButton functional component to mark 'none' as selected when book is not in a shelf

--- src/BookButton.js
+++ src/BookButton.js
@@ -1,12 +1,17 @@
 //Functional component to display dropdown option for each book
 
-const BookButton = (props) => (
-    <button 
+const BookButton = (props) => {
+    //add selected classes for the book and mark 'none' as selected if book is not in a shelf
+    let buttonClasses = props.book.shelf === props.shelf.shelfID ? "selected":"";
+    buttonClasses += (!props.book.shelf || props.book.shelf==='none') && (props.shelf.shelfID) === 'none' ? "selected":"";
+
+    return (<button 
         onClick={ (e) => props.moveBook(props.book, props.shelf.shelfID) }
-        className={props.book.shelf===props.shelf.shelfID ? "selected":""}
+        className={buttonClasses}
     >
         {props.shelf.shelfName}
     </button>
-)
+    )
+}
 
 export default BookButton;
